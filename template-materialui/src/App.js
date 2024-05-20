d24d9d5905341a67d30f79b7f7e1bc80412fb386
Lucasconceica <141232600+Lucasconceica@users.noreply.github.com>
2024-05-20 11:48:49 -0300

    first commit

---
+++ template-materialui/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import login from "./pages/tarefa/login";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+      <login />
+    </div>
+  );
+}
+
+export default App;
